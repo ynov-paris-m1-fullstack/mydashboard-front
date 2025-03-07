cbfef2b539288a2f45b3c1c53ec919f11929b6de
Vincent Zerbib <contact@zelabstudio.com>
2025-03-07 14:44:42 +0100

    save work

--- src/actions/auth/index.js
+++ src/actions/auth/index.js
@@ -15,5 +15,19 @@ export default async function register(state, formData) {
         }
     }
     await new Promise((resolve) => setTimeout(resolve, 1000));
+    const response = await fetch("/api/auth/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            firstName,
+            lastName,
+            email,
+            password
+        })
+    });
+    const data = await response.json();
+    console.log(data);
     redirect("/dashboard");
 }
